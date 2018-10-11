134335a3292b79601e5968bb998040d2b693577c
Sambit Kumar Giri <sgiri@astro.su.se>
2018-10-11 13:32:57 +0200

    Fixed Rs size

--- src/controler.f90
+++ src/controler.f90
@@ -16,7 +16,7 @@ real(8), dimension(nzl), intent(out) :: z_arr
 real(8), dimension(nzl, 4), intent(out) :: GLB_arr
 real(8), dimension(nkb), intent(out) :: k_arr
 real(8), dimension(nzl, nkb), intent(out) :: PSKZ_arr
-real(8), dimension(nzl, mbin), intent(out) :: BSDZRarr
+real(8), dimension(mbin), intent(out) :: BSDZRarr
 real(8), dimension(nzl, mbin), intent(out) :: BSDZ_arr
 
 call reionization_21cm(arr, zcut, z_arr, GLB_arr, k_arr, PSKZ_arr, BSDZRarr, BSDZ_arr)
